d5c6f65f80af660a8f1527f4f806b21f926e01d7
unknown <Saranthony@DESKTOP-VSCEOE9>
2023-02-21 15:43:16 +0100

    Ajout JSDoc

--- src/app/components/single-vehicule-personnel/single-vehicule-personnel.component.ts
+++ src/app/components/single-vehicule-personnel/single-vehicule-personnel.component.ts
@@ -2,6 +2,11 @@ import { Component, Input, OnInit } from '@angular/core';
 import { VehiculePersonnel } from 'src/app/models/vehicule-personnel';
 import { VehiculePersonnelService } from 'src/app/services/vehicule-personnel.service';
 
+/**
+ * Component permettant l'affichage d'un véhicule personnel
+ * individuel
+ * 
+ */
 @Component({
   selector: 'app-single-vehicule-personnel',
   templateUrl: './single-vehicule-personnel.component.html',
@@ -14,6 +19,12 @@ export class SingleVehiculePersonnelComponent {
   @Input()
   vehiculePersonnel!: VehiculePersonnel
 
+  /**
+   * Méthode de suppression d'un véhicule 
+   * déclenchée lors de l'évènement click sur le bouton de suppression
+   * 
+   * @param vehiculePersonnelId Id du véhicule à supprimer
+   */
   onDelete(vehiculePersonnelId?: number) {
     if (vehiculePersonnelId) {
       this.vehiculePersonnelService.deleteVehiculePersonnel(vehiculePersonnelId).subscribe();
--- src/app/services/vehicule-personnel.service.ts
+++ src/app/services/vehicule-personnel.service.ts
@@ -3,6 +3,12 @@ import { HttpClient } from "@angular/common/http";
 import { VehiculePersonnel } from '../models/vehicule-personnel';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
 
+/**
+ * 
+ * Méthodes de services CRUD requêtant l'API exposée par 
+ * Spring Boot pour les véhicules personnels
+ * 
+ */
 @Injectable({
   providedIn: 'root'
 })
@@ -12,15 +18,33 @@ export class VehiculePersonnelService {
 
   constructor(private http:HttpClient) { }
 
+  /**
+   * Requête POST de création d'un véhicule personnel
+   * 
+   * @param vehiculePersonnel Véhicule personnel à créer
+   * @returns 
+   */
   createVehiculePersonnel(vehiculePersonnel : VehiculePersonnel): Observable<VehiculePersonnel>{
     return this.http.post<VehiculePersonnel>('http://localhost:8080/rest/vehicule-personnel', vehiculePersonnel);
   }
 
+  /**
+   * Requête GET de recherche d'un véhicule personnel
+   * 
+   * @param collaborateurId 
+   * @returns 
+   */
   getVehiculePersonnelListByCollaborateurId(collaborateurId : number): Observable<VehiculePersonnel[]>{
     return this.http.get<VehiculePersonnel[]>(`http://localhost:8080/rest/vehicule-personnel/collaborateur/${collaborateurId}`).pipe(
       tap((vehiculePersonnel) => this.vehiculePersonnelListByCollaborateurId$.next(vehiculePersonnel)));
   }
 
+  /**
+   * Requête DELETE de suppression d'un véhicule personnel
+   * 
+   * @param vehiculePersonnelId 
+   * @returns 
+   */
   deleteVehiculePersonnel(vehiculePersonnelId: number): Observable<VehiculePersonnel>{
     return this.http.delete<VehiculePersonnel>(`http://localhost:8080/rest/vehicule-personnel/${vehiculePersonnelId}`).pipe(
       tap(() => this.vehiculePersonnelListByCollaborateurId$.next(
