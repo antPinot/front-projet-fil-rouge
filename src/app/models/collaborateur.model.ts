d1f39d3d5d49f977550d82edaccdd1bd4e7b9420
unknown <Saranthony@DESKTOP-VSCEOE9>
2023-02-22 17:47:53 +0100

    Gestion boutons suppression/reservation/details

---
+++ src/app/models/collaborateur.model.ts
@@ -0,0 +1,12 @@
+export interface Collaborateur{
+    id?: number,
+    matricule?: number,
+    nom?: string,
+    prenom?: string,
+    dateNaissance?: string,
+    telephone?: number,
+    mail?: string,
+    login?: string,
+    password?: string,
+    dateCreation?: string
+}
