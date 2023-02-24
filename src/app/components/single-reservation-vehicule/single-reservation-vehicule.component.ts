9c3c8351706ea258e12d34d8a61084562bccb984
unknown <pinot.anthony@gmail.com>
2023-02-24 17:34:41 +0100

    Suppression reservation vehicule service

--- src/app/components/single-reservation-vehicule/single-reservation-vehicule.component.ts
+++ src/app/components/single-reservation-vehicule/single-reservation-vehicule.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ReservationVehiculeSociete } from 'src/app/models/reservationVehiculeSociete.model';
+import { ReservationVehiculeService } from 'src/app/services/reservation-vehicule.service';
 
 @Component({
   selector: 'app-single-reservation-vehicule',
@@ -11,4 +12,14 @@ export class SingleReservationVehiculeComponent {
   @Input()
   reservationVehicule!: ReservationVehiculeSociete
 
+  constructor(private reservationVehiculeService: ReservationVehiculeService){}
+
+  onUpdate(){
+
+  }
+
+  onDelete(){
+    this.reservationVehiculeService.annulerReservationVehiculeSociete(this.reservationVehicule.id).subscribe()
+  }
+  
 }
