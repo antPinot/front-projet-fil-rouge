d6caab7744272213532e595091cdf73cfe3a7105
unknown <pinot.anthony@gmail.com>
2023-02-28 11:26:24 +0100

    JSDoc header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,6 +3,12 @@ import { Router } from '@angular/router';
 import { tap } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 
+/** 
+ * 
+ * Component gérant le menu de navigation 
+ * et la déconnexion du collaborateur
+ * 
+ */
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -12,6 +18,7 @@ export class HeaderComponent {
 
   constructor(private authService: AuthService, private router:Router){ }
 
+  /** Gère la déconnexion : Vide le local storage et redirige vers le component de connexion */
   onLogout(){
     this.authService.logout().pipe(
       tap(() => localStorage.clear()),
