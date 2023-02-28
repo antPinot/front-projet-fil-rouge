93007354d7c71f32ed4e3812382bd1bebf0ad4f5
unknown <pinot.anthony@gmail.com>
2023-02-28 11:35:00 +0100

    JSDoc edit-reservation-vehicule

--- src/app/components/edit-reservation-vehicule/edit-reservation-vehicule.component.ts
+++ src/app/components/edit-reservation-vehicule/edit-reservation-vehicule.component.ts
@@ -2,10 +2,16 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { tap } from 'rxjs';
-import { VehiculeSociete } from 'src/app/models/vehicule-societe';
 import { AuthService } from 'src/app/services/auth.service';
 import { ReservationVehiculeService } from 'src/app/services/reservation-vehicule.service';
 
+/**
+ * Component gérant la modification d'une réservation de véhicule de société
+ * 
+ * Attention : Ce component n'est pas entièrement fonctionnel et des problèmes de cohérence
+ * dans la persistance des données peuvent exister. Il faudra implémenter les contrôles adéquats.
+ * 
+ */
 @Component({
   selector: 'app-edit-reservation-vehicule',
   templateUrl: './edit-reservation-vehicule.component.html',
@@ -13,30 +19,39 @@ import { ReservationVehiculeService } from 'src/app/services/reservation-vehicul
 })
 export class EditReservationVehiculeComponent implements OnInit, OnDestroy {
 
+  /** Id du collaborateur connecté */
   collaborateurId?= this.authService.currentCollaborateur?.id;
 
+  /** Réservation de véhicule de société à éditer, récupérée depuis le service */
   reservationVehiculeToEdit = this.reservationVehiculeService.reservationVehiculeSocieteToEdit;
 
+  /** Liste des véhicules de société disponibles */
   listVehiculeSociete$ = this.reservationVehiculeService.listVehicule$
 
+  /** Formulaire de modification d'une réservation de véhicule de société */
   reservationVehiculeEditForm!: FormGroup
 
+  /** Véhicule de société faisant l'objet de la réservation à modifier */
   currentVehiculeSociete = this.reservationVehiculeService.reservationVehiculeSocieteToEdit.vehiculeSociete;
 
   constructor(private reservationVehiculeService: ReservationVehiculeService, private formBuilder: FormBuilder, private router: Router, private authService:AuthService) { }
 
+  /** Initialisation du formulaire */
   ngOnInit(): void {
     this.reservationVehiculeEditForm = this.formBuilder.group({
       dateDepart: [this.reservationVehiculeToEdit.dateDepart, Validators.required],
       dateRetour: [this.reservationVehiculeToEdit.dateRetour, Validators.required],
     })
+    // Comportement à revoir éventuellement
     this.reservationVehiculeService.getAllVehiculeSociete().subscribe()
   }
 
+  /** Réinitialisaton de la liste des véhicules de société disponibles à la destruction du component*/
   ngOnDestroy(): void {
     this.listVehiculeSociete$.next([]);
   }
 
+  /** Fonction "précédent" du carrousel */
   previous() {
     if (this.currentVehiculeSociete) {
       let index = this.listVehiculeSociete$.value.indexOf(this.currentVehiculeSociete)
@@ -49,6 +64,7 @@ export class EditReservationVehiculeComponent implements OnInit, OnDestroy {
     }
   }
 
+  /** Fonction "suivant" du carrousel */
   next() {
     if (this.currentVehiculeSociete) {
       let index = this.listVehiculeSociete$.value.indexOf(this.currentVehiculeSociete)
@@ -61,6 +77,7 @@ export class EditReservationVehiculeComponent implements OnInit, OnDestroy {
     }
   }
 
+  /** Soumission du formulaire */
   onSubmitForm() {
     this.reservationVehiculeToEdit.dateDepart = this.reservationVehiculeEditForm.value.dateDepart;
     this.reservationVehiculeToEdit.dateRetour = this.reservationVehiculeEditForm.value.dateRetour;
