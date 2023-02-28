0c62a071b8696d78829c674d1870e555fab8df89
unknown <pinot.anthony@gmail.com>
2023-02-28 11:43:43 +0100

    JSDoc search-covoiturage-component

--- src/app/components/search-covoiturage/search-covoiturage.component.ts
+++ src/app/components/search-covoiturage/search-covoiturage.component.ts
@@ -5,6 +5,10 @@ import * as moment from 'moment';
 import { AuthService } from 'src/app/services/auth.service';
 import { ReservationCovoiturageService } from 'src/app/services/reservation-covoiturage.service';
 
+/**
+ * Component gérant la recherche d'un covoiturage
+ * 
+ */
 @Component({
   selector: 'app-search-covoiturage',
   templateUrl: './search-covoiturage.component.html',
@@ -12,28 +16,34 @@ import { ReservationCovoiturageService } from 'src/app/services/reservation-covo
 })
 export class SearchCovoiturageComponent implements OnInit, OnDestroy {
 
+  /** Id du collaborateur connecté */
   collaborateurId?= this.authService.currentCollaborateur?.id;
 
+  /** Formulaire de recherche */
   searchForm!: FormGroup;
 
+  /** Résultat de la recherche : Liste des covoiturages */
   searchResults$ = this.reservationCovoiturageService.listCovoiturageByDateDepart$;
 
+  /** Date courante : Minimum pour effectuer une recherche par date */
   minDate = new Date();
 
   constructor(private reservationCovoiturageService: ReservationCovoiturageService, private formBuilder: FormBuilder, private authService: AuthService,
-    private adapter: DateAdapter<any>, @Inject(MAT_DATE_LOCALE) private locale:string) { }
+    private adapter: DateAdapter<any>, @Inject(MAT_DATE_LOCALE) private locale: string) { }
 
-
+  /** Initialisation du formulaire */
   ngOnInit(): void {
     this.searchForm = this.formBuilder.group({
       searchDateDepart: [null]
     })
   }
 
+  /** Réinitialisation de la liste des covoiturages disponbiles à la destruction du component */
   ngOnDestroy(): void {
     this.searchResults$.next([]);
   }
 
+  /** Recherche d'un covoiturage par date, formatage de la date récupérér par datetimepicker (Angular Material) via moment.js*/
   onSearch() {
     if (this.collaborateurId) {
       let formattedDate = moment(this.searchForm.value.searchDateDepart).format("DD/MM/YYYY");
