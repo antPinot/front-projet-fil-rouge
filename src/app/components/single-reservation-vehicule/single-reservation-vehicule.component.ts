14ea9e4b1103f56c3efda0e4bf75057393b80857
unknown <pinot.anthony@gmail.com>
2023-02-28 12:00:06 +0100

    JSDoc single-reservation-vehicule

--- src/app/components/single-reservation-vehicule/single-reservation-vehicule.component.ts
+++ src/app/components/single-reservation-vehicule/single-reservation-vehicule.component.ts
@@ -3,6 +3,11 @@ import { Router } from '@angular/router';
 import { ReservationVehiculeSociete } from 'src/app/models/reservationVehiculeSociete.model';
 import { ReservationVehiculeService } from 'src/app/services/reservation-vehicule.service';
 
+/**
+ * Component gérant l'affichage d'une réservation individuelle d'un véhicule
+ * de société
+ * 
+ */
 @Component({
   selector: 'app-single-reservation-vehicule',
   templateUrl: './single-reservation-vehicule.component.html',
@@ -10,16 +15,19 @@ import { ReservationVehiculeService } from 'src/app/services/reservation-vehicul
 })
 export class SingleReservationVehiculeComponent{
 
+   /** Réservation de véhicule de société émise par l'observable du component parent (list-reservation-vehicule-societe) */
   @Input()
   reservationVehicule!: ReservationVehiculeSociete
 
   constructor(private reservationVehiculeService: ReservationVehiculeService, private router:Router){}
 
+  /** Affecte le véhicule à modifier dans le service correspondant et redirige vers le formulaire de modification*/
   onEdit(){
     this.reservationVehiculeService.reservationVehiculeSocieteToEdit = this.reservationVehicule;
     this.router.navigateByUrl('vehicule-societe/reservation/edit')
   }
 
+  /** Annule une réservation de véhicule de société */
   onDelete(){
     this.reservationVehiculeService.annulerReservationVehiculeSociete(this.reservationVehicule.id).subscribe()
   }
