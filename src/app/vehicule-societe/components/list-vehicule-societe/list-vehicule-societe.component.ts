db5e4ab00a98b8321a44641631f2a3f79a20722c
unknown <pinot.anthony@gmail.com>
2024-02-29 15:32:45 +0100

    Ajout filtre immatriculation

--- src/app/vehicule-societe/components/list-vehicule-societe/list-vehicule-societe.component.ts
+++ src/app/vehicule-societe/components/list-vehicule-societe/list-vehicule-societe.component.ts
@@ -11,7 +11,11 @@ import { VehiculeSocieteService } from '../../../core/services/vehicule-societe.
 })
 export class ListVehiculeSocieteComponent {
 
+  /**Directive ViewChild donnant accès au composant de filtre par Marque */
   @ViewChild('selectMarque') selectMarque!: MatSelect
+
+  /**Directive ViewChild donnant accès au composant de filtre par Immatriculation */
+  @ViewChild('selectImmatriculation') selectImmatriculation!: MatSelect
 
   vSList$ = this._vehiculeSocieteService.vehiculesSociete$;
 
@@ -19,24 +23,53 @@ export class ListVehiculeSocieteComponent {
 
   allMarques!: (string |undefined )[]
 
+  allImmatriculations!: (string |undefined )[]
+
   constructor(private _vehiculeSocieteService: VehiculeSocieteService) { };
 
   ngOnInit() {
     this._vehiculeSocieteService.findAllVehiculeSociete().pipe(tap(() => {
       this.allMarques = this.vSList$.value.map((v) => v.marque)
+      this.allImmatriculations = this.vSList$.value.map((v) => v.immatriculation)
       this.filteredList$.next(this.vSList$.value)
     })).subscribe();
     
   }
 
-  filterOnMarque(marque : string){
-    if (this.selectMarque.value != null){
-      this.filteredList$.next(this._vehiculeSocieteService.vehiculesSociete$.value.filter((v) => v.marque === marque));
-    }
+  // filterOnMarque(marque : string){
+  //   if (this.selectMarque.value != null){
+  //     this.filteredList$.next(this._vehiculeSocieteService.vehiculesSociete$.value.filter((v) => v.marque === marque));
+  //   }
+  // }
+
+  // filterOnImmatriculation(immatriculation : string){
+  //   if (this.selectImmatriculation.value != null){
+  //     this.filteredList$.next(this._vehiculeSocieteService.vehiculesSociete$.value.filter((v) => v.immatriculation === immatriculation));
+  //   }
+  // }
+
+  filter(){
+    this.filteredList$.next(this._vehiculeSocieteService.vehiculesSociete$.value.filter((v) =>{
+      if (this.selectImmatriculation.value != null){
+        if (v.immatriculation != this.selectImmatriculation.value){
+          return false;
+        }
+      }
+      return true;
+    })
+    .filter((v) => {
+      if (this.selectMarque.value != null){
+        if (v.marque != this.selectMarque.value){
+          return false;
+        }
+      }
+      return true;
+    }));
   }
 
   onReinit(){
     this.selectMarque.value = null
+    this.selectImmatriculation.value = null
     this.filteredList$.next(this.vSList$.value)
   }
 
